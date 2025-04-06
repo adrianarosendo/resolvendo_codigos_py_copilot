ae88e6628949b270b19a855f531efcfe566852cc
adrianarosendo <adriana.maiate@gmail.com>
2025-04-06 11:31:43 -0300

    Incluindo repetidor e concatenando

---
+++ concatenando.py
@@ -0,0 +1,9 @@
+# Recebe os dados 
+info1 = input("Digite o nome do primeiro usuário: ")
+info2 = input("Digite o nome do segundo usuário: ")
+
+# Concatena os dados em uma string
+mensagem = f"{info1} {info2}."
+
+# Exibe a string concatenada
+print(mensagem)
---
+++ repetidor.py
@@ -0,0 +1,12 @@
+# Solicita uma string ao usuário
+texto = input("Digite uma palavra ou frase: ")
+
+# Solicita um número inteiro
+vezes = int(input("Digite um número inteiro: "))
+
+# Repete a string o número de vezes informado
+montagem = texto + " "
+resultado = montagem * vezes
+
+# Exibe o resultado
+print(resultado)
